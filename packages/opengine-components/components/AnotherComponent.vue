e7e85a434ea431c5341222f98f90832169c5e6e5
Tiago Velho <tiago.velho@l1.com>
2024-01-03 11:48:38 +0000

    style: fix linting

--- packages/opengine-components/components/AnotherComponent.vue
+++ packages/opengine-components/components/AnotherComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2>Component Repo</h2>
-    <p class="bg-black text-white">Hello, this is ANOTHER shared component.</p>
+    <p class="bg-black text-white">
+      Hello, this is ANOTHER shared component.
+    </p>
   </div>
 </template>
